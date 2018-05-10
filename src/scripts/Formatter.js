95c289e493105af18315c03e393d57d4b284155b
baskew <b.askew@exeter.ac.uk>
2018-05-10 08:35:52 +0100

    Extract data formatting methods to own class

---
+++ src/scripts/Formatter.js
@@ -0,0 +1,69 @@
+class Formatter
+{
+     /**
+     * @param {object} fields
+     * @return {object}
+     */
+    prepareFields(fields) {
+        // Have to copy object to avoid manipulating original data stored in state
+        const queryFields = JSON.parse(JSON.stringify(fields));
+
+        this.stripEmptyFields(queryFields);
+        this.removeSpacesFromTags(queryFields);
+        this.validateMinMaxValues(queryFields);
+
+        return queryFields;
+    }
+
+    /**
+     * @param {object} fields
+     * @return {object}
+     */
+    stripEmptyFields(fields) {
+        for (const key in fields) {
+            if (!fields[key] || fields[key] === '') {
+                delete fields[key];
+            }
+        }
+    }
+
+    /**
+     * @param {object}
+     */
+    removeSpacesFromTags(fields) {
+        if (fields.hasOwnProperty('tags')) {
+            fields.tags = fields.tags.replace(/\s/g, '');
+        }
+    }
+
+    /**
+     * @param {object} fields
+     */
+    validateMinMaxValues(fields) {
+        if (this.minMoreThanMax(fields, 'minDuration', 'maxDuration')) {
+            throw Error('Minimum duration must be less than maximum duration');
+        }
+
+        if (this.minMoreThanMax(fields, 'minRate', 'maxRate')) {
+            throw Error('Minimum rate must be less than maximum rate');
+        }
+    }
+
+    /**
+     * @param {object} fields
+     * @param {string} min
+     * @param {string} max
+     * @return {bool}
+     */
+    minMoreThanMax(fields, min, max) {
+        if (fields.hasOwnProperty(min) && fields.hasOwnProperty(max)) {
+            if (fields[min] > fields[max]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+export default new Formatter();
--- src/scripts/Jobs.js
+++ src/scripts/Jobs.js
@@ -1,5 +1,6 @@
 import Request from './Request';
 
+import Formatter from './Formatter';
 import Tags from './Tags';
 
 class Jobs {
@@ -13,7 +14,7 @@ class Jobs {
         let queryFields = null;
 
         try {
-            queryFields = this.prepareFields(fields);
+            queryFields = Formatter.prepareFields(fields);
         } catch (error) {
             return {error: error.message};
         }
@@ -50,7 +51,7 @@ class Jobs {
         let queryFields = null;
 
         try {
-            queryFields = this.prepareFields(jobInformation);
+            queryFields = Formatter.prepareFields(jobInformation);
             queryFields.tags = queryFields.tags.split(',');
         } catch (error) {
             return {error: error.message};
@@ -76,71 +77,6 @@ class Jobs {
         } catch (error) {
             return {error: 'Unable to process related jobs'};
         }
-    }
-
-    /**
-     * @param {object} fields
-     * @return {object}
-     */
-    prepareFields(fields) {
-        // Have to copy object to avoid manipulating original data stored in state
-        const queryFields = JSON.parse(JSON.stringify(fields));
-
-        this.stripEmptyFields(queryFields);
-        this.removeSpacesFromTags(queryFields);
-        this.validateMinMaxValues(queryFields);
-
-        return queryFields;
-    }
-
-    /**
-     * @param {object} fields
-     * @return {object}
-     */
-    stripEmptyFields(fields) {
-        for (const key in fields) {
-            if (!fields[key] || fields[key] === '') {
-                delete fields[key];
-            }
-        }
-    }
-
-    /**
-     * @param {object}
-     */
-    removeSpacesFromTags(fields) {
-        if (fields.hasOwnProperty('tags')) {
-            fields.tags = fields.tags.replace(/\s/g, '');
-        }
-    }
-
-    /**
-     * @param {object} fields
-     */
-    validateMinMaxValues(fields) {
-        if (this.minMoreThanMax(fields, 'minDuration', 'maxDuration')) {
-            throw Error('Minimum duration must be less than maximum duration');
-        }
-
-        if (this.minMoreThanMax(fields, 'minRate', 'maxRate')) {
-            throw Error('Minimum rate must be less than maximum rate');
-        }
-    }
-
-    /**
-     * @param {object} fields
-     * @param {string} min
-     * @param {string} max
-     * @return {bool}
-     */
-    minMoreThanMax(fields, min, max) {
-        if (fields.hasOwnProperty(min) && fields.hasOwnProperty(max)) {
-            if (fields[min] > fields[max]) {
-                return true;
-            }
-        }
-
-        return false;
     }
 }
 
